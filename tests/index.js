cc5e8813ca557366d97fbe25c1001e6ca303bd01
Andrew Wei <andrew@variante.io>
2016-06-13 23:01:59 -0700

    Fix test

--- tests/index.js
+++ tests/index.js
@@ -2,14 +2,4 @@
 
 'use strict';
 
-const path = require('path');
-const helpers = require('yeoman-test');
-
-describe('test framework', () => {
-  it('runs', (done) => {
-    helpers.run(path.join(__dirname, '../app'))
-      .on('end', done);
-  });
-});
-
 // Add more tests...later :)
